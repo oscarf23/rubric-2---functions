b03d445016c62af856d8af258c5b63edfbf6dd58
oscarf23 <oscarmf23@gmail.com>
2020-02-07 16:52:16 -0600

    first try

---
+++ teachers.js
@@ -0,0 +1,16 @@
+
+alert("welcome to my rubric 2 functions page");
+
+var teacherName=prompt(who is your teacher?);
+	
+switch(teacherName)
+{case( "Oscar"}:
+alert("Correct!");
+break;
+		
+case "Bob":
+alert("Wow, even better!");
+break;
+		
+default:
+alert("That is not a teacher");// JavaScript Document
